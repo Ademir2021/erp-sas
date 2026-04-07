3841d0335552a7d0fb8eee5e7c3a6d872c920221
Ademir Souza de Almeida <centroserra@gmail.com>
2026-04-07 16:18:12 -0300

    save codes

--- app/components/AccountsReceivable/AccountsReceivableForm.tsx
+++ app/components/AccountsReceivable/AccountsReceivableForm.tsx
@@ -15,15 +15,18 @@ export default function AccountsReceivableForm({
     setReceipt, receipt
 }: Props) {
 
+    const styles_input = "w-full p-2 border rounded-lg"
+
     return <>
-        <p>AccountsReceivableForm</p>
-        <div className="max-w-3xl mx-auto bg-gray-600 p-8 rounded-2xl shadow-lg">
-            <form className="">
+        <p className="text-xl font-bold text-center">Contas a Receber</p>
+        {accountsReceivable.length > 0 ? <div className="max-w-3xl mx-auto bg-gray-600 p-8 rounded-2xl shadow-lg">
+            <form className="flex flex-col items-center gap-4" onSubmit={handleSubmit}>
+                <label>Informe o Valor a Receber</label>
                 <input
-                    className="w-2x5 p-1 border rounded-lg"
+                    className={`${styles_input}`}
                     type="number"
                     value={receipt.receipt || ''}
-                    placeholder="Valor a Receber"
+                    placeholder="Ex: 100.00"
                     onChange={(e) =>
                         setReceipt({
                             ...receipt,
@@ -31,10 +34,10 @@ export default function AccountsReceivableForm({
                         })}
                 />
                  <input
-                    className="w-2x5 p-1 border rounded-lg"
+                    className={`${styles_input}`}
                     type="number"
                     value={receipt.discount || ''}
-                    placeholder="Desconto"
+                    placeholder="Desconto Concedido - (Ex: 10.00)"
                     onChange={(e) =>
                         setReceipt({
                             ...receipt,
@@ -42,7 +45,7 @@ export default function AccountsReceivableForm({
                         })}
                 />
             </form>
-        </div>
+        </div> : <p className="flex justify-center p-4 text-green-300 ">Nenhuma conta a receber encontrada.</p>}
         {msg && <p>{msg}</p>}
         <AccountsReceivableList
             accountsReceivable={accountsReceivable}
