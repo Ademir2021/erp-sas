5c122bafaa27fbc50edd6bff67a50f467b611b56
Ademir Souza de Almeida <centroserra@gmail.com>
2026-04-17 15:05:54 -0300

    save codes

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -4,9 +4,9 @@ export default function About() {
     <main className="p-6 text-white">
       <h1 className="text-2xl font-bold mb-4">Sobre o ERP</h1>
     <hr/>
-      <p className="p-3">Versão: {pkg.version}</p>
+      <p className="mt-2">Versão: {pkg.version}</p>
 
-      <p className="text-gray-500 font-bold mb-3">ERP - SAS</p>
+      <p className=" text-gray-500 font-bold">Nome: {pkg.name.toUpperCase()}</p>
 
       <p className="text-gray-100 leading-relaxed">
        Esta aplicação ERP foi desenvolvida para gerenciamento de dados e processos,
