6dd2515c15350835bd618673cadcad906b630abc
Ademir Souza de Almeida <centroserra@gmail.com>
2026-04-22 11:52:04 -0300

    save codes

--- app/components/login/LoginForm.tsx
+++ app/components/login/LoginForm.tsx
@@ -2,6 +2,8 @@
 
 import { TLogin } from "@/app/models/TUser"
 import Link from "next/link"
+import Logo from "../Logo"
+import { globalsStyles_login_btn, globalsStyles_login_div, globalsStyles_login_div_hight, globalsStyles_login_div_left, globalsStyles_login_input, globalsStyles_login_main } from "../GlobalStyles"
 
 type Props = {
     children: TLogin
@@ -17,56 +19,62 @@ export default function LoginForm({
     handleChange,
     error, msg }: Props) {
     return (
-    
-        <div className="flex min-h-screen bg-transparent items-center justify-center text-gray-100">
-            <div className="w-full max-w-md bg-black/60 p-8 rounded-2xl shadow-lg">
-                <p className="flex justify-center text-gray-300 font-bold text-2xl p-1 mb-6">Sign in.</p>
-                <form onSubmit={handleLogin} className="space-y-4 ">
-                    <input
-                        type="email"
-                        name='login'
-                        placeholder="Email"
-                        value={children.login}
-                        required
-                        onChange={handleChange}
-                        className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Senha"
-                        value={children.password}
-                        required
-                        onChange={handleChange}
-                        className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
-                    />
-                    <button
-                        type="submit"
-                        className="w-full bg-blue-600 text-white py-3 rounded-lg font-semibold hover:bg-blue-700 transition duration-200 cursor-pointer"
-                    >
-                        Entrar
-                    </button>
-                </form>
+        <div className={`${globalsStyles_login_div}`}>
+            <main className={`${globalsStyles_login_main}`}>
+                {/* Lateral Esquerdo*/}
+                <div className={`${globalsStyles_login_div_left}`}>
+                    <div className="text-center text-blue-400"><Logo /></div>
+                    <p className="font-normal  text-lg ml-12">Entre com sua Conta!</p>
+                    <span className="text-center">Gerencie a gestão de sua Empresa com a <br />
+                        segurança que só o ERP-SAS te oferece.</span>
+                </div>
 
-                {(error || msg) && (<p className={`mt-4 text-sm text-center
+                <div className={`${globalsStyles_login_div_hight}`}>
+                    <p className="flex justify-center text-gray-300 font-bold text-2xl p-1 mb-6">Sign in.</p>
+                    <form onSubmit={handleLogin} className="space-y-4 ">
+                        <input
+                            type="email"
+                            name='login'
+                            placeholder="Email"
+                            value={children.login}
+                            required
+                            onChange={handleChange}
+                            className={`${globalsStyles_login_input}`}
+                        />
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Senha"
+                            value={children.password}
+                            required
+                            onChange={handleChange}
+                            className={`${globalsStyles_login_input}`}
+                        />
+                        {(error || msg) && (<p className={`text-sm text-center
                 ${error ? "text-red-600" : "text-blue-700"}`}>
-                    {error || msg}</p>
-                )}
-                <Link className="text-gray-300 pe-2" href={'/register'}>Registrar-seu Login</Link>
-                {/* <div className="min-h-screenxx flex items-center justify-center p-3">
+                            {error || msg}</p>
+                        )}
+                        <button type="submit"
+                            className={`${globalsStyles_login_btn}`}>
+                            Entrar</button>
+                    </form>
+
+                    <Link className="text-start" href={'/register'}>Registrar-seu Login</Link>
+                    {/* <div className="min-h-screenxx flex items-center justify-center p-3">
                     <button
                         onClick={() => signIn("github", { callbackUrl: '/dashboard' })}
                         className="bg-black text-white px-6 py-3 rounded-lg cursor-pointer"
                     >Entrar com GitHub</button>
                 </div> */}
-                {/* <div className="min-h-screenxx flex items-center justify-center p-3">
+                    {/* <div className="min-h-screenxx flex items-center justify-center p-3">
                     <button
                         onClick={() => signIn("google", { callbackUrl: '/dashboard' })}
                         className="bg-black text-white px-6 py-3 rounded-lg cursor-pointer"
                     >Entrar com google</button>
                 </div> */}
-                <p className="text-end"><Link href='/' >Sair</Link></p>
-            </div>
+                    <p className="text-end"><Link href='/' >Sair</Link></p>
+                </div>
+            </main>
         </div>
     )
 }
--- app/components/login/RegisterLoginForm.tsx
+++ app/components/login/RegisterLoginForm.tsx
@@ -4,6 +4,7 @@ import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 import { TUser, UserRole } from "@/app/models/TUser"
 import Link from "next/link"
 import { useState } from "react"
+import { globalsStyles_login_btn, globalsStyles_login_div, globalsStyles_login_div_hight, globalsStyles_login_div_left, globalsStyles_login_input, globalsStyles_login_main } from '../GlobalStyles';
 
 type Props = {
   children: TUser
@@ -35,11 +36,11 @@ export default function RegisterLoginForm({
           className="flex cursor-pointer ">
             <KeyboardReturnIcon titleAccess='Voltar'/></button>}</>
   return (
-    <div className="min-h-screen bg-transparent flex items-center justify-center p-4">
-      <div className="w-full max-w-4xl bg-black/60 rounded-2xl shadow-lg overflow-hidden flex flex-col md:flex-row">
+    <div className={`${globalsStyles_login_div}`}>
+      <main className={`${globalsStyles_login_main}`}>
 
         {/* Lateral Esquerda - Progresso */}
-        <div className="md:w-1/2  text-white flex flex-col justify-center p-8 space-y-6">
+        <div className={`${globalsStyles_login_div_left}`}>
           {steps.map((title, index) => {
             const currentStep = index + 1
             const isActive = step === currentStep
@@ -74,7 +75,7 @@ export default function RegisterLoginForm({
           </div>
 
         {/* Lado Direito */}
-        <div className="md:w-1/2 p-8">
+        <div className={`${globalsStyles_login_div_hight}`}>
           <h2 className="text-2xl font-semibold mb-6 text-gray-300">
             {steps[step - 1]}
           </h2>
@@ -89,7 +90,7 @@ export default function RegisterLoginForm({
                   onChange={handleChange}
                   value={children.login}
                   placeholder="Digite seu email"
-                  className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className={`${globalsStyles_login_input}`}
                 />
               </>
             )}
@@ -117,7 +118,7 @@ export default function RegisterLoginForm({
                   onChange={handleChange}
                   value={children.password}
                   placeholder="Digite sua senha"
-                  className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+                 className={`${globalsStyles_login_input}`}
                 />
               </>
             )}
@@ -126,7 +127,7 @@ export default function RegisterLoginForm({
           {msg && back}
           <button
            onClick= {step !==3 ? nextStep : handleSubmit}
-            className="mt-6 w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition"
+            className={`${globalsStyles_login_btn}`}
           >
             {step === 3 ? "Finalizar" : "Próximo"}
           </button>
@@ -134,7 +135,7 @@ export default function RegisterLoginForm({
             <Link className=" text-end" href={'/login'}>Logar</Link>
             </p>
         </div>
-      </div>
+      </main>
     </div>
   )
 }
