c035b81bc1fba1ae7b944b1b9dfcf9c762352c6b
Ademir Souza de Almeida <centroserra@gmail.com>
2026-05-20 13:48:07 -0300

    save codes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,17 +9,22 @@ export default function Footer() {
                 <p>Todos os direitos reservados 2021 - {year}</p>
             </div>
             <div className="flex items-center justify-center gap-4 mb-2 mt-2">
-                <span className="text-sm text-white">Powered by: Centro Informática. </span>
+                <div className="text text-white">
+                <img
+                    src="/logos/logo_centroinfo_new.png"
+                    alt="Power By"
+                    className="h-12 w-auto rounded"
+                /> </div>
                 <img
                     src="/logos/github.png"
                     alt="Logo 1"
-                    className="h-8 w-auto border border-white/60 rounded"
+                    className="h-6 w-auto border border-white/60 rounded"
                 />
 
                 <img
                     src="/logos/nginx.jpg"
                     alt="Logo 2"
-                    className="h-8 w-auto border border-white/60 rounded"
+                    className="h-6 w-auto border border-white/60 rounded"
                 />
 
                 <img
